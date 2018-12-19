5c246a211d7a7cf265dcfdd21545c6766f99db7f
Nigel David <nigel.a.david@gmail.com>
2018-12-19 16:17:58 -0600

    New changes to resume and revert accidental commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', []);
 
 app.controller('commonController', function($scope) {
 	$scope.title = "Nigel David - Portfolio";
-	$scope.resumePath = "docs/Nigel_David_Resume_2017.pdf";
+	$scope.resumePath = "docs/Nigel_David_Resume_2018.pdf";
 
 	//Helper functions
 	$scope.exists = function (property){
@@ -24,7 +24,7 @@ app.controller('aboutController', function($scope) {
 	$scope.getEmailHref = function(email) {
 		return "mailto:" + email;
 	};
-}); 
+});
 
 app.controller("experienceController", function($scope) {
 	$scope.experienceCollection = [
@@ -32,7 +32,7 @@ app.controller("experienceController", function($scope) {
 					dates: "April 2016 – Present",
 					project: "Software Defined Networks – GUI Portal",
 					position: "Software Developer",
-					company: "IBM",					
+					company: "IBM",
 					client: "AT&T Account",
 					description: "Created and enhanced components of AT&T’s SDN Web Portal. Coded the front end components using HTML, JS, and the DHTMLX (JS, AJAX, HTML) framework which presented the user with a functional GUI. Coded the back end components were Java and Spring which were used for data processing and REST API handling. The system ran MariaDb to maintain customer provisioning data. Ran Fortify Scan and fixed security vulnerabilities. Wrote backend unit tests using JUnit and Mockito.",
 					toolsUsed: "Websphere, SVN, XMLSpy, MySQL Workbench, Eclipse, CA Agile, Rational Team Concert, Bitbucket, Git, JIRA, Tomcat, Maven"
@@ -40,7 +40,7 @@ app.controller("experienceController", function($scope) {
 				{
 					dates: "June 2014 – March 2016",
 					position: "Scrum Master",
-					company: "IBM",						
+					company: "IBM",
 					client: "AT&T Account",
 					description: "Served as a leader for scrum teams to ensure Agile methodologies and principles are adhered to throughout iterations, prevent interference from outside the Scrum team, and provide assistance to the team when potential blockers are present. Organize and facilitate sprint planning, daily stand-up meetings, reviews, retrospectives, release planning, demos and other Scrum related meetings. Coordinate deliverables across iterations and releases and act as a liaison between the Product Owners and the team."
 				},
@@ -48,11 +48,11 @@ app.controller("experienceController", function($scope) {
 					dates: "October 2013 – June 2015",
 					project: "Universal Service Request Platform",
 					position: "System Engineer",
-					company: "IBM",						
+					company: "IBM",
 					client: "AT&T Account",
 					description: "Served as a leader for scrum teams to ensure Agile methodologies and principles are adhered to throughout iterations, prevent interference from outside the Scrum team, and provide assistance to the team when potential blockers are present. Organize and facilitate sprint planning, daily stand-up meetings, reviews, retrospectives, release planning, demos and other Scrum related meetings. Coordinate deliverables across iterations and releases and act as a liaison between the Product Owners and the team.",
 					toolsUsed: "Word, Excel macros, VI, Eclipse, XML Spy, iRise, Sybase, ReqPro, RallyDev"
-				}								
+				}
 	];
 
 	//Helper functions
